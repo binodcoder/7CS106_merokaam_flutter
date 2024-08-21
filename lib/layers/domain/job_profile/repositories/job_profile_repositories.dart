01ede6ef7b58d7c2e8e8cfc41327e48230f33c31
binodcoder <binodcoder@gmail.com>
2024-08-21 15:52:12 +0100

    test for usecase DeleteJobProfile done

--- lib/layers/domain/job_profile/repositories/job_profile_repositories.dart
+++ lib/layers/domain/job_profile/repositories/job_profile_repositories.dart
@@ -4,7 +4,7 @@ import '../../../../../core/errors/failures.dart';
 
 abstract class JobProfileRepository {
   Future<Either<Failure, List<JobProfile>>>? readJobProfiles();
-  Future<Either<Failure, int>>? deleteJobProfile(JobProfile post);
-  Future<Either<Failure, int>>? createJobProfile(JobProfile post);
-  Future<Either<Failure, int>>? updateJobProfile(JobProfile post);
+  Future<Either<Failure, int>>? deleteJobProfile(JobProfile jobProfile);
+  Future<Either<Failure, int>>? createJobProfile(JobProfile jobProfile);
+  Future<Either<Failure, int>>? updateJobProfile(JobProfile jobProfile);
 }
--- test/layers/domain/routine/usecases/delete_job_profile_test.dart
+++ test/layers/domain/routine/usecases/delete_job_profile_test.dart
@@ -1,39 +1,44 @@
-import 'package:fitness_app/core/model/routine_model.dart';
-import 'package:fitness_app/layers/domain/routine/repositories/routine_repositories.dart';
-import 'package:fitness_app/layers/domain/routine/usecases/delete_routine.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:merokaam/core/models/job_profile_model.dart';
+import 'package:merokaam/layers/domain/job_profile/repositories/job_profile_repositories.dart';
+import 'package:merokaam/layers/domain/job_profile/usecases/delete_job_profile.dart';
 import 'package:mockito/mockito.dart';
 import 'package:dartz/dartz.dart';
 
-class MockRoutineRepository extends Mock implements RoutineRepository {}
+class MockJobProfileRepository extends Mock implements JobProfileRepository {}
 
 void main() {
-  late DeleteRoutine usecase;
-  late MockRoutineRepository mockRoutineRepository;
+  late DeleteJobProfile usecase;
+  late MockJobProfileRepository mockJobProfileRepository;
   setUp(() {
-    mockRoutineRepository = MockRoutineRepository();
-    usecase = DeleteRoutine(mockRoutineRepository);
+    mockJobProfileRepository = MockJobProfileRepository();
+    usecase = DeleteJobProfile(mockJobProfileRepository);
   });
 
   int tResponse = 1;
 
-  RoutineModel tRoutineModel = RoutineModel(
-    id: 1,
-    description: '',
-    difficulty: '',
-    duration: 0,
-    name: '',
-    source: '',
+  JobProfileModel tJobProfileModel = JobProfileModel(
+    userAccountId: 0,
+    firstName: '',
+    lastName: '',
+    city: '',
+    state: '',
+    country: '',
+    workAuthorization: '',
+    employmentType: '',
+    resume: '',
+    profilePhoto: '',
+    photosImagePath: '',
   );
 
   test(
     'should get int from the repository',
     () async {
-      when(mockRoutineRepository.deleteRoutine(tRoutineModel.id!)).thenAnswer((_) async => Right(tResponse));
-      final result = await usecase(tRoutineModel.id!);
+      when(mockJobProfileRepository.deleteJobProfile(tJobProfileModel!)).thenAnswer((_) async => Right(tResponse));
+      final result = await usecase(tJobProfileModel!);
       expect(result, Right(tResponse));
-      verify(mockRoutineRepository.deleteRoutine(tRoutineModel.id!));
-      verifyNoMoreInteractions(mockRoutineRepository);
+      verify(mockJobProfileRepository.deleteJobProfile(tJobProfileModel!));
+      verifyNoMoreInteractions(mockJobProfileRepository);
     },
   );
 }
