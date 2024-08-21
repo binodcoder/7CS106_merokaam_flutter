5d2ad30a71391b311109ef4c61d53c5281814d13
binodcoder <binodcoder@gmail.com>
2024-08-21 16:37:33 +0100

    test for usecase UpdateJobProfile is done

--- test/layers/domain/routine/usecases/update_job_profile_test.dart
+++ test/layers/domain/routine/usecases/update_job_profile_test.dart
@@ -1,39 +1,44 @@
-import 'package:fitness_app/core/model/routine_model.dart';
-import 'package:fitness_app/layers/domain/routine/repositories/routine_repositories.dart';
-import 'package:fitness_app/layers/domain/routine/usecases/update_routine.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:merokaam/core/models/job_profile_model.dart';
+import 'package:merokaam/layers/domain/job_profile/repositories/job_profile_repositories.dart';
+import 'package:merokaam/layers/domain/job_profile/usecases/update_job_profile.dart';
 import 'package:mockito/mockito.dart';
 import 'package:dartz/dartz.dart';
 
-class MockRoutineRepository extends Mock implements RoutineRepository {}
+class MockJobProfileRepository extends Mock implements JobProfileRepository {}
 
 void main() {
-  late UpdateRoutine usecase;
-  late MockRoutineRepository mockRoutineRepository;
+  late UpdateJobProfile usecase;
+  late MockJobProfileRepository mockJobProfileRepository;
   setUp(() {
-    mockRoutineRepository = MockRoutineRepository();
-    usecase = UpdateRoutine(mockRoutineRepository);
+    mockJobProfileRepository = MockJobProfileRepository();
+    usecase = UpdateJobProfile(mockJobProfileRepository);
   });
 
   int tResponse = 1;
 
-  RoutineModel tRoutineModel = RoutineModel(
-    id: 1,
-    description: '',
-    difficulty: '',
-    duration: 0,
-    name: '',
-    source: '',
+  JobProfileModel tJobProfileModel = JobProfileModel(
+    userAccountId: 0,
+    firstName: '',
+    lastName: '',
+    city: '',
+    state: '',
+    country: '',
+    workAuthorization: '',
+    employmentType: '',
+    resume: '',
+    profilePhoto: '',
+    photosImagePath: '',
   );
 
   test(
     'should get int from the repository',
     () async {
-      when(mockRoutineRepository.updateRoutine(tRoutineModel)).thenAnswer((_) async => Right(tResponse));
-      final result = await usecase(tRoutineModel);
+      when(mockJobProfileRepository.updateJobProfile(tJobProfileModel)).thenAnswer((_) async => Right(tResponse));
+      final result = await usecase(tJobProfileModel);
       expect(result, Right(tResponse));
-      verify(mockRoutineRepository.updateRoutine(tRoutineModel));
-      verifyNoMoreInteractions(mockRoutineRepository);
+      verify(mockJobProfileRepository.updateJobProfile(tJobProfileModel));
+      verifyNoMoreInteractions(mockJobProfileRepository);
     },
   );
 }
