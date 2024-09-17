89f5a79fed9efc06832ebaa0fe615282202b4777
binodcoder <binodcoder@gmail.com>
2024-09-17 22:09:54 +0100

    centralised all Urls

---
+++ lib/resources/url.dart
@@ -0,0 +1,12 @@
+class AppUrl {
+  static const String baseUrl = "http://192.168.1.180:5000";
+
+  // auth
+  static const String signin = "$baseUrl/auth/signin";
+  static const String signup = "$baseUrl/auth/signup";
+
+  // job-profile
+  static const String jobProfile = "$baseUrl/api/job-profile";
+  static const String create = "$baseUrl/api/create";
+  static const String profile = "$baseUrl/api/profile";
+}
